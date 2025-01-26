7d6dcc53611b34c2acf40bd136a324751f7c85ed
wowitsalison <aliatmra@gmail.com>
2025-01-25 21:49:05 -0600

    Update movegen.h

--- include/movegen.h
+++ include/movegen.h
@@ -4,5 +4,5 @@
 
 // Pawn moves
 uint64_t generatePawnSinglePush(uint64_t pawns, uint64_t empty_squares, bool isWhite);
-uint16_t generatePawnDoublePush(uint64_t pawns, uint64_t empty_squares, bool isWhite);
+uint64_t generatePawnDoublePush(uint64_t pawns, uint64_t empty_squares, bool isWhite);
 uint64_t generatePawnCaptures(uint64_t pawns, uint64_t enemy_peices, bool isWhite);
