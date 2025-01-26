5841c17b4c4bea3a4e6eb1fdd8a1286b86410e68
wowitsalison <aliatmra@gmail.com>
2025-01-26 00:51:49 -0600

    Update movegen.h to include all piece moves

--- include/movegen.h
+++ include/movegen.h
@@ -6,3 +6,13 @@
 uint64_t generatePawnSinglePush(uint64_t pawns, uint64_t empty_squares, bool isWhite);
 uint64_t generatePawnDoublePush(uint64_t pawns, uint64_t empty_squares, bool isWhite);
 uint64_t generatePawnCaptures(uint64_t pawns, uint64_t enemy_peices, bool isWhite);
+// Bishop moves
+uint64_t generateBishopMoves(uint64_t bishops, uint64_t empty_squares, uint64_t enemy_pieces);
+// Knight moves
+uint64_t generateKnightMoves(uint64_t knights, uint64_t empty_squares, uint64_t enemy_pieces);
+// Rook moves
+uint64_t generateRookMoves(uint64_t rooks, uint64_t empty_squares, uint64_t enemy_pieces);
+// Queen moves
+uint64_t generateQueenMoves(uint64_t queens, uint64_t empty_squares, uint64_t enemy_pieces);
+// King moves
+uint64_t generateKingMoves(uint64_t kings, uint64_t empty_squares, uint64_t enemy_pieces);
