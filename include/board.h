07ba4facc0b207a4982265b386c6a9f131f56947
wowitsalison <aliatmra@gmail.com>
2025-01-26 00:29:54 -0600

    Add board masks

--- include/board.h
+++ include/board.h
@@ -12,5 +12,20 @@ public:
 };
 
 // Board masks
-const uint64_t FILE_A = 0x0101010101010101ULL; // File A mask
-const uint64_t FILE_H = 0x8080808080808080ULL; // File H mask
+const uint64_t FILE_A = 0x0101010101010101ULL;
+const uint64_t FILE_B = FILE_A << 1;
+const uint64_t FILE_C = FILE_A << 2;
+const uint64_t FILE_D = FILE_A << 3;
+const uint64_t FILE_E = FILE_A << 4;
+const uint64_t FILE_F = FILE_A << 5;
+const uint64_t FILE_G = FILE_A << 6;
+const uint64_t FILE_H = FILE_A << 7;
+
+const uint64_t RANK_1 = 0x00000000000000FFULL;
+const uint64_t RANK_2 = RANK_1 << 8;
+const uint64_t RANK_3 = RANK_1 << 16;
+const uint64_t RANK_4 = RANK_1 << 24;
+const uint64_t RANK_5 = RANK_1 << 32;
+const uint64_t RANK_6 = RANK_1 << 40;
+const uint64_t RANK_7 = RANK_1 << 48;
+const uint64_t RANK_8 = RANK_1 << 56;
