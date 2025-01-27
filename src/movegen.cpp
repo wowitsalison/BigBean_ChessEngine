2e8abcfa722abbf7485adaf9854464ed83b86265
wowitsalison <aliatmra@gmail.com>
2025-01-26 22:20:10 -0600

    Integrate logMoves in pawn move generation functions

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -3,34 +3,49 @@
 
 // Move pawn one square forward
 uint64_t generatePawnSinglePush(uint64_t pawns, uint64_t empty_squares, bool isWhite) {
+    uint64_t move = 0;
     if (isWhite) {
-        return (pawns << 8) & empty_squares;
+        move = (pawns << 8) & empty_squares;
+        logMoves(move);
+        return move;
     } else {
-        return (pawns >> 8) & empty_squares;
+        move = (pawns >> 8) & empty_squares;
+        logMoves(move);
+        return move;
     }
 }
 
 // Move pawn two squares forward from starting rank
 uint64_t generatePawnDoublePush(uint64_t pawns, uint64_t empty_squares, bool isWhite) {
+    uint64_t moves = 0;
     if (isWhite) {
         uint64_t singlePush = generatePawnSinglePush(pawns, empty_squares, true);
-        return (singlePush << 8) & empty_squares & 0x00000000FF000000ULL; // Ranks 2 - 4
+        moves = (singlePush << 8) & empty_squares & 0x00000000FF000000ULL; // Ranks 2 - 4;
+        logMoves(moves);
+        return moves;
     } else {
         uint64_t singlePush = generatePawnSinglePush(pawns, empty_squares, false);
-        return (singlePush >> 8) & empty_squares & 0x000000FF00000000ULL; // Ranks 5 - 7
+        moves = (singlePush >> 8) & empty_squares & 0x000000FF00000000ULL; // Ranks 5 - 7
+        logMoves(moves);
+        return moves;
     }
 }
 
 // Capture piece with pawn diagonally
 uint64_t generatePawnCaptures(uint64_t pawns, uint64_t enemy_peices, bool isWhite) {
+    uint64_t move = 0;
     if (isWhite) {
         uint64_t leftCapture = (pawns << 7) & ~FILE_A & enemy_peices;
         uint64_t rightCapture = (pawns << 9) & ~FILE_H & enemy_peices;
-        return leftCapture | rightCapture;
+        move = leftCapture | rightCapture;
+        logMoves(move);
+        return move;
     } else {
         uint64_t leftCapture = (pawns >> 9) & ~FILE_H & enemy_peices;
         uint64_t rightCapture = (pawns >> 7) & ~FILE_A & enemy_peices;
-        return leftCapture | rightCapture;
+        move = leftCapture | rightCapture;
+        logMoves(move);
+        return move;
     }
 }
 
