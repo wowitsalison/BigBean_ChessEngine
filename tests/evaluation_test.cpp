3417680d4c57c9ac2c3091e9d33c231c10887e88
wowitsalison <aliatmra@gmail.com>
2025-01-30 14:41:51 -0600

    Create evaluation_test.cpp to test evaluation.cpp

---
+++ tests/evaluation_test.cpp
@@ -0,0 +1,83 @@
+#include <cassert>
+#include <iostream>
+#include "utils.h"
+#include "board.h"
+#include "gameState.h"
+
+class ChessTest {
+private:
+    GameState gs;
+    int score = 0;
+    
+    void printTestResult(const std::string& testName, bool passed) {
+        std::cout << testName << ": " << (passed ? "PASSED" : "FAILED") << std::endl;
+    }
+
+    void printMove(const Move& move) {
+        std::cout << "  " << squareToAlgebraic(move.sourceSquare) 
+                 << " -> " << squareToAlgebraic(move.destinationSquare) << std::endl;
+    }
+
+    bool testEmptyBoard() {
+        gs.initialize("8/8/8/8/8/8/8/8 w - - 0 1"); // Initialize empty board
+
+        // Evaluate board
+        score = gs.board.evaluate();
+        std::cout << "\nEvaluating board:";
+
+        bool passed = true;
+        passed &= (score == 0);
+
+        printTestResult("\nEmpty board test", passed);
+        return passed;
+    }
+
+    bool testOnePawnUp() {
+        gs.initialize("2k5/1p6/8/5P2/6Pp/8/3P4/5K2 w - - 0 1");
+
+        // Evaluate board
+        score = gs.board.evaluate();
+        std::cout << "\nEvaluating board:";
+
+        bool passed = true;
+        passed &= (score == 1);
+        std::cout << "\nActual score: " << score;
+
+        printTestResult("\nOne pawn up test", passed);
+        return passed;
+    }
+
+    bool testRookVKnight() {
+        gs.initialize("2k5/1p6/8/5P2/6Pp/3n4/1R1P4/5K2 w - - 0 1");
+
+        // Evaluate board
+        score = gs.board.evaluate();
+        std::cout << "\nEvaluating board:";
+
+        bool passed = true;
+        passed &= (score > 0);
+        std::cout << "\nActual score: " << score;
+
+        printTestResult("\nRook vs Knight test", passed);
+        return passed;
+    }
+
+
+public:
+    void runAllTests() {
+        std::cout << "Running chess engine tests...\n" << std::endl;
+        
+        bool allPassed = true;
+        allPassed &= testEmptyBoard();
+        allPassed &= testOnePawnUp();
+        allPassed &= testRookVKnight();
+        
+        std::cout << "\nOverall test result: " << (allPassed ? "ALL PASSED" : "SOME FAILED") << std::endl;
+    }
+};
+
+int main() {
+    ChessTest tester;
+    tester.runAllTests();
+    return 0;
+}
