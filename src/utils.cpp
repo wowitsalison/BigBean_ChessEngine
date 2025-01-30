acc5ea2bce3d4e64b3f756e296ce360dac5d5fe2
wowitsalison <aliatmra@gmail.com>
2025-01-29 22:08:26 -0600

    Fix algebraicToSquare rank and file calculations

--- src/utils.cpp
+++ src/utils.cpp
@@ -11,13 +11,13 @@ int algebraicToSquare(const std::string& algebraic) {
 
     char file = algebraic[0];
     char rank = algebraic[1];
-    
-    if (file < 'a' || file > 'h' || rank <'1' || rank > '8') {
+
+    if (file < 'a' || file > 'h' || rank < '1' || rank > '8') {
         throw std::invalid_argument("Invalid algebraic notation");
     }
 
     int fileInt = file - 'a';
-    int rankInt = rank - '1';
+    int rankInt = 7 - (rank - '1'); 
 
     return rankInt * 8 + fileInt;
 }
