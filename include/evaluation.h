0d1fd9fd875e7e6fb57e8709eb615b5cfc5eecf7
wowitsalison <aliatmra@gmail.com>
2025-01-30 00:19:14 -0600

    Calculate initial piece values

---
+++ include/evaluation.h
@@ -0,0 +1,10 @@
+
+
+// Values calculated by taking Larry Kaufman's values from 1999 and adjusting to
+// have the ratio be more similar to the ratio of Stockfish's piece values
+const int PAWN_VALUE = 100;
+const int KNIGHT_VALUE = 370;
+const int BISHOP_VALUE = 390;
+const int ROOK_VALUE = 600;
+const int QUEEN_VALUE = 1000;
+
