06de4dd63423f23c2e717a798287f5c2b68be15a
wowitsalison <aliatmra@gmail.com>
2025-01-30 23:42:08 -0600

    Add tests for combined position and material evaluation

--- tests/evaluation_test.cpp
+++ tests/evaluation_test.cpp
@@ -22,7 +22,7 @@ private:
         gs.initialize("8/8/8/8/8/8/8/8 w - - 0 1"); // Initialize empty board
 
         // Evaluate board
-        score = gs.board.evaluate();
+        score = gs.board.materialEvaluation();
         std::cout << "\nEvaluating board:";
 
         bool passed = true;
@@ -36,7 +36,7 @@ private:
         gs.initialize("2k5/1p6/8/5P2/6Pp/8/3P4/5K2 w - - 0 1");
 
         // Evaluate board
-        score = gs.board.evaluate();
+        score = gs.board.materialEvaluation();
         std::cout << "\nEvaluating board:";
 
         bool passed = true;
@@ -51,7 +51,7 @@ private:
         gs.initialize("2k5/1p6/8/5P2/6Pp/3n4/1R1P4/5K2 w - - 0 1");
 
         // Evaluate board
-        score = gs.board.evaluate();
+        score = gs.board.materialEvaluation();
         std::cout << "\nEvaluating board:";
 
         bool passed = true;
@@ -62,9 +62,34 @@ private:
         return passed;
     }
 
+    bool testInitialBoard() {
+        gs.initialize();
+
+        score = gs.board.evaluate();
+
+        bool passed = true;
+        passed &= (score == 0);
+        std::cout << "\nActual score: " << score;
+
+        printTestResult("\nStarting position test", passed);
+        return passed;
+    }
+
+    bool testWhiteAdvantage() {
+        gs.initialize("1k3r2/2R3pp/5p2/8/8/8/5PPP/6K1 w - - 0 1");
+
+        score = gs.board.evaluate();
+
+        bool passed = true;
+        passed &= (score > 1);
+        std::cout << "\nActual score: " << score;
+
+        printTestResult("\nWhite Advantage test", passed);
+        return passed;
+    }
 
 public:
-    void runAllTests() {
+    void runMaterialTests() {
         std::cout << "Running chess engine tests...\n" << std::endl;
         
         bool allPassed = true;
@@ -74,10 +99,20 @@ public:
         
         std::cout << "\nOverall test result: " << (allPassed ? "ALL PASSED" : "SOME FAILED") << std::endl;
     }
+
+    void runCombinedTests() {
+        std::cout << "Running chess engine tests...\n" << std::endl;
+
+        bool allPassed = true;
+        allPassed &= testInitialBoard();
+        allPassed &= testWhiteAdvantage();
+        
+        std::cout << "\nOverall test result: " << (allPassed ? "ALL PASSED" : "SOME FAILED") << std::endl;
+    }
 };
 
 int main() {
     ChessTest tester;
-    tester.runAllTests();
+    tester.runCombinedTests();
     return 0;
 }
