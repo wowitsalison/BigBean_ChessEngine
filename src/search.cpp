1f480c52695b724a4a1553f5a7ab6eaf82d20fce
wowitsalison <aliatmra@gmail.com>
2025-03-06 13:16:43 -0600

    Create initial minimax function

--- src/search.cpp
+++ src/search.cpp
@@ -1,7 +1,33 @@
+#include <iostream>
 #include "search.h"
-#include "gameState.h"
 
 int minimax(GameState gs, int depth, bool maximizingPlayer) {
+    if (depth == 0) return gs.board.evaluate();
 
-    // Put stuff here
+    std::vector<Move> legalMoves = gs.generateLegalMoves();
+
+    if (legalMoves.empty()) {
+        if (gs.isCheckmate()) return maximizingPlayer ? -100000 : 100000;
+        if (gs.isStalemate()) return 0;
+    }
+
+    if (maximizingPlayer) {
+        int maxEval = -100000;
+        for (const Move& move : legalMoves) {
+            gs.makeMove(move);
+            int eval = minimax(gs, depth - 1, false);
+            gs.undoMove();
+            maxEval = std::max(maxEval, eval);
+        }
+        return maxEval;
+    } else {
+        int minEval = 100000;
+        for (const Move& move : legalMoves) {
+            gs.makeMove(move);
+            int eval = minimax(gs, depth - 1, true);
+            gs.undoMove();
+            minEval = std::min(minEval, eval);
+        }
+        return minEval;
+    }
 }
