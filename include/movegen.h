b46e970a8090e6e5f9d7e25b70d1b4dcd69d27c9
Alison <aliatmra@gmail.com>
2025-09-05 18:50:13 -0500

    Add declaration for generateSlidingAttacks

--- include/movegen.h
+++ include/movegen.h
@@ -48,6 +48,7 @@ uint64_t generateBishopAttacks(uint64_t bishops, uint64_t allPieces);
 uint64_t generateRookAttacks(uint64_t rooks, uint64_t allPieces);
 uint64_t generateQueenAttacks(uint64_t queens, uint64_t allPieces);
 uint64_t generateKingAttacks(uint64_t king);
+uint64_t generateSlidingAttacks(int square, uint64_t allPieces, const int directions[]);
 
 std::vector<Move> generateAllMoves(const Board& board, Side side);
 std::vector<Move> bitboardToMoves(uint64_t fromPieces, uint64_t toSquares, char piece);
